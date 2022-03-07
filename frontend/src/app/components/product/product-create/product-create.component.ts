1b48e929981846f2345083086743039f7afc7f8a
Cristiano Brum <Cristianolbrum@gmail.com>
2022-03-07 19:28:22 -0400

    aula 15 - metodo de criar produtos

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,13 +1,19 @@
 import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router'
+import { Product } from '../product.model';
 
 @Component({
   selector: 'app-product-create',
   templateUrl: './product-create.component.html',
   styleUrls: ['./product-create.component.css']
 })
-export class ProductCreateComponent implements OnInit {
+export class ProductCreateComponent implements OnInit{
+
+  product: Product = {
+    name : '',
+    price: 0
+  }
 
   constructor(private productService: ProductService, private router: Router) { }
 
@@ -16,7 +22,10 @@ export class ProductCreateComponent implements OnInit {
   }
 
   createProduct(): void{
-    this.productService.showMessage('Operação executada com sucesso');
+    this.productService.create(this.product).subscribe(() => {
+      this.productService.showMessage('Produto cadastrado com sucesso')
+      this.router.navigate(['/products'])
+    })
   }
 
   cancel(): void{
